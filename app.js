08b17374a177c82cd6d04ca6d16e13e2bb32fb54
wazeefz <em2@em2s-MacBook-Pro.local>
2024-01-06 12:12:43 +0800

    second push

---
+++ app.js
@@ -0,0 +1,35 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            firstName: 'Wazeef',
+            lastName: 'Zainol',
+            url: 'https://google.com',
+            raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+            age: 20,
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLastName(event) {
+            this.lastName = event.target.value
+        },
+    },
+}).mount('#app')
+
+// setTimeout(() => {
+//     vm.firstName = 'Bob'
+// }, 2000)
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Milla',
+//             lastName: 'Ridzuan'
+//         }
+//     },
+// }).mount('#app2')
